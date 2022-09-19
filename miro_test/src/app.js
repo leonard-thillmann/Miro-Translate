7500a04306239648054fe171a222fa777ce872c3
Jannes Blobel <72493222+jannesblobel@users.noreply.github.com>
2022-09-19 17:22:46 +0200

    optimize working code

--- miro_test/src/app.js
+++ miro_test/src/app.js
@@ -3,77 +3,25 @@ translate.from = "ger" //Set a language to translate from
 let language = "eng"; //Set a language to translate into
 
 
-
 // Get all items from the board
 const items = await miro.board.getSelection();
 
-let stickyContentTrimmed = "";
-let stickyContentArray = [];
-let cardContentTrimmed = "";
-let cardContentArray = [];
-
-//const stickyNote = await miro.board.getSelection();
-
-//console.log(sticky)
-//stickyNote[0].content = "Leo"
-
-
-async function getStickyNotes() {
-
-  let i = 0;
-  let j = 0;
-
+async function translation() {
   items.forEach(async (items) => {
     switch (items.type) {
       case 'sticky_note': //For every selected Stickynote
         if (items.content) {
-          stickyContentTrimmed = items.content.replace(/<\/?[^>]+(>|$)/g, "");
-          stickyContentArray.push({ content: stickyContentTrimmed, id: items.id, index: i })
-          i++;
-          console.log(items)
-          items.content = await translate(stickyContentTrimmed, language) //Content wird replaced
-          items.sync();
-        }
+          items.content = await translate(items.content.replace(/<\/?[^>]+(>|$)/g, ""), language) //Content remove html Tag and 
+        } 
       case 'card': //For every selected card
         if(items.title) {
-          cardContentTrimmed = items.title.replace(/<\/?[^>]+(>|$)/g, "");
-          cardContentArray.push({ title: cardContentTrimmed, index: j })
+          items.title = await translate(items.title.replace(/<\/?[^>]+(>|$)/g, ""), language) //Content remove html Tag and    
+          items.description = await translate(items.description.replace(/<\/?[^>]+(>|$)/g, ""), language)
         }
     }
-  });
-  stickyContentArray.forEach(async element => createStickynote(element))
-  cardContentArray.forEach(async element => createCard(element))  
-}
-
-/* ################ STICKYNOTES ################################################################################ */
-
-async function createStickynote(element) {
-  const stickyNote = await miro.board.createStickyNote({
-    content: await translate(element.content, language),
-    style: {
-      fillColor: 'red'
-    },
-    width: 1000,
-    x: element.index*1000 //Setting the stickyNote off by its index*1000
-  });
-  //stickyNote.content = "TESSST"
-}
-
-/* ############## CARDS ######################################################################################### */
-
-async function createCard(element) {
-  const card = await miro.board.createCard({
-    title: await translate(element.title, language),
-    width: 1000,
-    height: 1000,
-    x: element.index*1000 //Setting the stickyNote off by its index*1000
+    items.sync();
   });
 }
 
+await translation();
 
-//console.log(stickyContentArray)
-await getStickyNotes();
-
-//console.log(document.getElementById("3458764533559056179"))
-
-//await stickyNote.sync();
