ee44c0827d9cdf764230c70a72a3c8f64238d783
hfg-leonard-t <leonard.thillmann@hfg.design>
2022-09-21 17:05:25 +0200

    added more languages

--- miro_translation/src/index.js
+++ miro_translation/src/index.js
@@ -2,7 +2,7 @@ async function init() {
   miro.board.ui.on('icon:click', async () => {
     await miro.board.ui.openPanel({
       url: 'app.html',
-      height: 475,
+      height: 800,
     });
   });
 }
