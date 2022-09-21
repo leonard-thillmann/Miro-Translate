0a213b23604a1efee21efe65dbd1975998a496e0
hfg-leonard-t <leonard.thillmann@hfg.design>
2022-09-21 12:32:53 +0200

    Implemented working language selection.

--- miro_test/src/app.js
+++ miro_test/src/app.js
@@ -1,10 +1,14 @@
-translate.engine = "google"; // Or "yandex", "libre", "deepl"
-translate.from = "ger" //Set a language to translate from
-let language = "eng"; //Set a language to translate into
+let fromLanguage = ""
+let intoLanguage = ""
 
 const htmlTag = '/<\/?[^>]+(>|$)/g'
 
+
+
 async function translation() {
+
+  translate.engine = "google"; // Or "yandex", "libre", "deepl"
+  translate.from = fromLanguage //Set a language to translate from
 
   // Get all items from the board
   const items = await miro.board.getSelection();
@@ -13,42 +17,67 @@ async function translation() {
     switch (items.type) {
       case 'sticky_note': //For every selected Stickynote
         if (items.content) {
-          items.content = await translate(items.content.replace(htmlTag, ""), language) //Content remove html Tag and 
+          items.content = await translate(items.content.replace(htmlTag, ""), intoLanguage) //Content remove html Tag and 
         }
       case 'card': //For every selected card
         //TODO runtime optimisation  IF statement
         if (items.title) {
-          items.title = await translate(items.title.replace(htmlTag, ""), language) //Content remove html Tag and    
-          items.description = await translate(items.description.replace(htmlTag, ""), language)
+          items.title = await translate(items.title.replace(htmlTag, ""), intoLanguage) //Content remove html Tag and    
+          items.description = await translate(items.description.replace(htmlTag, ""), intoLanguage)
         }
       case 'text':
         if (items.content) {
-          items.content = await translate(items.content.replace(htmlTag, ""), language) //Content remove html Tag and    
+          items.content = await translate(items.content.replace(htmlTag, ""), intoLanguage) //Content remove html Tag and    
         }
       case 'app_card':
         if (items.title) {
-          items.title = await translate(items.title.replace(htmlTag, ""), language) //Content remove html Tag and 
-          items.description = await translate(items.description.replace(htmlTag, ""), language)
+          items.title = await translate(items.title.replace(htmlTag, ""), intoLanguage) //Content remove html Tag and 
+          items.description = await translate(items.description.replace(htmlTag, ""), intoLanguage)
         }
       case 'shape':
         if (items.content) {
-          items.content = await translate(items.content.replace(htmlTag, ""), language) //Content remove html Tag and    
+          items.content = await translate(items.content.replace(htmlTag, ""), intoLanguage) //Content remove html Tag and    
         }
       case 'frame':
         if (items.title) {
-          items.title = await translate(items.title.replace(htmlTag, ""), language) //Content remove html Tag and 
+          items.title = await translate(items.title.replace(htmlTag, ""), intoLanguage) //Content remove html Tag and 
         }
       case 'image':
         if (items.title) {
-          items.title = await translate(items.title.replace(htmlTag, ""), language) //Content remove html Tag and 
+          items.title = await translate(items.title.replace(htmlTag, ""), intoLanguage) //Content remove html Tag and 
         }
     }
     items.sync();
   });
+  console.log("TRIED TO TRANSLATE")
 }
 
-function testFunction() {
-  console.log("TEST")
+
+
+function changeIntoLanguage() {
+  if (intoLanguage === "eng") {
+    intoLanguage = "ger"
+    console.log("INTOLANGUAGE: ", intoLanguage)
+  } else {
+    intoLanguage = "eng"
+    console.log("INTOLANGUAGE: ", intoLanguage)
+  }
 }
+
+function changeFromLanguage() {
+  if (fromLanguage === "eng") {
+    fromLanguage = "ger"
+    console.log("FROMLANGUAGE: ", fromLanguage)
+  } else {
+    fromLanguage = "eng"
+    console.log("FROMLANGUAGE: ", fromLanguage)
+  }
+}
+
+function langCheck() {
+  console.log("intoLanguage: ", intoLanguage, "fromLanguage: ", fromLanguage)
+}
+
+console.log("intoLanguage: ", intoLanguage, "fromLanguage: ", fromLanguage)
 
 //await translation();
