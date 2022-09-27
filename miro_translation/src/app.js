c941e21d2569e075984e844a48d08f95324fe380
hfg-leonard-t <leonard.thillmann@hfg.design>
2022-09-27 11:42:42 +0200

    integrated copy & paste, doesnt translate yet

--- miro_translation/src/app.js
+++ miro_translation/src/app.js
@@ -10,47 +10,112 @@ async function translation() {
   translate.engine = "google"; // Or "yandex", "libre", "deepl"
   translate.from = fromLanguage //Set a language to translate from
 
-  // Get all items from the board
+  //############################################# EDIT ########################################################################
+
+  // Get all selected items
   const items = await miro.board.getSelection();
 
-  items.forEach(async (items) => {
-    switch (items.type) {
-      case 'sticky_note': //For every selected Stickynote
-        if (items.content) {
-          items.content = await translate(items.content.replace(htmlTag, ""), intoLanguage) //Content remove html Tag and 
-        }
-      case 'card': //For every selected card
-        //TODO runtime optimisation  IF statement
-        if (items.title) {
-          items.title = await translate(items.title.replace(htmlTag, ""), intoLanguage) //Content remove html Tag and    
-          items.description = await translate(items.description.replace(htmlTag, ""), intoLanguage)
-        }
+  items.forEach(async (item) => {
+    switch (item.type) {
+      case 'sticky_note':
+        item.content = await translate(item.content.replace(htmlTag, ""), intoLanguage) //Remove html Tag and translate content
+        break;
+      case 'card':
+        item.title = await translate(item.title.replace(htmlTag, ""), intoLanguage) //Remove html Tag and translate content  
+        item.description = await translate(item.description.replace(htmlTag, ""), intoLanguage)
+        break;
       case 'text':
-        if (items.content) {
-          items.content = await translate(items.content.replace(htmlTag, ""), intoLanguage) //Content remove html Tag and    
-        }
+        item.content = await translate(item.content.replace(htmlTag, ""), intoLanguage) //Remove html Tag and translate content
+        break;
       case 'app_card':
-        if (items.title) {
-          items.title = await translate(items.title.replace(htmlTag, ""), intoLanguage) //Content remove html Tag and 
-          items.description = await translate(items.description.replace(htmlTag, ""), intoLanguage)
-        }
+        item.title = await translate(item.title.replace(htmlTag, ""), intoLanguage) //Remove html Tag and translate content
+        item.description = await translate(item.description.replace(htmlTag, ""), intoLanguage)
+        break;
       case 'shape':
-        if (items.content) {
-          items.content = await translate(items.content.replace(htmlTag, ""), intoLanguage) //Content remove html Tag and    
-        }
+        item.content = await translate(item.content.replace(htmlTag, ""), intoLanguage) //Remove html Tag and translate content
+        break;
       case 'frame':
-        if (items.title) {
-          items.title = await translate(items.title.replace(htmlTag, ""), intoLanguage) //Content remove html Tag and 
-        }
+        item.title = await translate(item.title.replace(htmlTag, ""), intoLanguage) //Remove html Tag and translate content
+        break;
       case 'image':
-        if (items.title) {
-          items.title = await translate(items.title.replace(htmlTag, ""), intoLanguage) //Content remove html Tag and 
-        }
+        item.title = await translate(item.title.replace(htmlTag, ""), intoLanguage) //Remove html Tag and translate content
+        break;
     }
-    items.sync();
+    item.sync();
   });
-  //console.log("TRIED TO TRANSLATE")
 }
+
+
+
+//########################################################## COPY PASTE ##########################################################
+
+let elementsWithCoords = [];
+let rightElementX = "";
+
+async function translationRight() {
+
+  translate.engine = "google"; //Or "yandex", "libre", "deepl"
+  translate.from = fromLanguage //Set a language to translate from
+
+  let board = await miro.board.getSelection();
+
+  //Creates an array with just the elements that have a x-value
+  board.forEach(element => {
+    if (element.x) {
+      if (element.type != "table_text") {
+        elementsWithCoords.push(element)
+      }
+    }
+  })
+
+  //The x-value of the most right item on the board
+  let rightElement = elementsWithCoords.reduce((prev, current) => prev.x > current.x ? prev : current)
+  rightElementX = rightElement.x + rightElement.width
+
+  //The x-value of the most left item on the board
+  let leftElementX = elementsWithCoords.reduce((prev, current) => prev.x < current.x ? prev : current).x
+  console.log(leftElementX, rightElementX)
+
+  const items = await miro.board.getSelection();
+
+  //Creates new, similar elements on the right side of the board
+  items.forEach(async (item) => {
+    switch (item.type) {
+      case 'sticky_note':
+        await miro.board.createStickyNote({
+          ...item, //Uses the same values as the original Stickynote
+          x: rightElementX + item.x - leftElementX,
+          height: undefined
+        });
+
+      case 'card': //For every selected card
+        item.title = await translate(item.title.replace(htmlTag, ""), intoLanguage) //Content remove html Tag and
+        item.description = await translate(item.description.replace(htmlTag, ""), intoLanguage)
+        break;
+      case 'text':
+        item.content = await translate(item.content.replace(htmlTag, ""), intoLanguage) //Content remove html Tag and
+        break;
+      case 'app_card':
+        item.title = await translate(item.title.replace(htmlTag, ""), intoLanguage) //Content remove html Tag and
+        item.description = await translate(item.description.replace(htmlTag, ""), intoLanguage)
+        break;
+      case 'shape':
+        item.content = await translate(item.content.replace(htmlTag, ""), intoLanguage) //Content remove html Tag and
+        break;
+      case 'frame':
+        item.title = await translate(item.title.replace(htmlTag, ""), intoLanguage) //Content remove html Tag and
+        break;
+      case 'image':
+        item.title = await translate(item.title.replace(htmlTag, ""), intoLanguage) //Content remove html Tag and
+        break;
+    }
+    item.sync();
+  });
+}
+
+
+
+//########################################### CHANGE LANGUAGES ######################################################
 
 async function changeFromLanguage(event) {
   console.log(event.target.id)
@@ -66,13 +131,9 @@ function changeIntoLanguage(event) {
 
 function swapLanguages() {
   document.getElementById("fromLanguage").innerHTML = document.getElementById(intoLanguage).innerHTML
-  
   document.getElementById("intoLanguage").innerHTML = document.getElementById(fromLanguage).innerHTML
-
   //Swap languages
   temp = fromLanguage;
   fromLanguage = intoLanguage;
   intoLanguage = temp;
 }
-
-//console.log("intoLanguage: ", intoLanguage, "fromLanguage: ", fromLanguage)
